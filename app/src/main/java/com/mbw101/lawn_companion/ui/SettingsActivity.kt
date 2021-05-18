a6cd7f6071e89fb8ca9783e8efb779c3c8a22862
Malcolm Wright <wrightmbw01@gmail.com>
2021-05-18 18:33:24 -0400

    Changed settings screen title. Converted main activity to a relative layout instead of a constraint layout, so I didn't need to specify a height dp for the fragment view.

--- app/src/main/java/com/mbw101/lawn_companion/ui/SettingsActivity.kt
+++ app/src/main/java/com/mbw101/lawn_companion/ui/SettingsActivity.kt
@@ -9,7 +9,7 @@ import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceFragmentCompat
 import com.mbw101.lawn_companion.R
 
-class SettingsActivity : AppCompatActivity() {
+class SettingsActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,8 +21,14 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
 
+        init()
+    }
+
+    private fun init() {
+        // theme the action bar
         supportActionBar?.setDisplayHomeAsUpEnabled(true) // allows back button
         supportActionBar?.setBackgroundDrawable(ColorDrawable(ContextCompat.getColor(MyApplication.applicationContext(), R.color.background_black))) // background black colour
+        supportActionBar?.title = getString(R.string.settingsTitle)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -38,6 +44,7 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
+
         override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey)
         }
